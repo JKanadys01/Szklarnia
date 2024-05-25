0600762995f9fa14b9a26eb550b749e144dd511b
JKanadys01 <jakubkanadys001@gmail.com>
2024-05-25 23:48:12 +0200

    dodanie klas

---
+++ Alarm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenHouse
+{
+    internal class Alarm
+    {
+        public int id;
+        public int alarm_parameter_id;
+        public int record_id;
+
+        public Alarm(int id, int alarm_parameter_id, int record_id)
+        {
+            this.id = id;
+            this.alarm_parameter_id = alarm_parameter_id;
+            this.record_id = record_id;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {id}\nAlarm parameter ID: {alarm_parameter_id}\nRecord Id: {record_id}\n";
+        }
+
+    }
+}
---
+++ AlarmParameter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenHouse
+{
+    internal class AlarmParameter
+    {
+        public int id;
+        public int device_id;
+        public string parameter;
+        public int min_value;
+        public int max_value;
+
+        public AlarmParameter(int id, int device_id, string parameter, int min_value, int max_value)
+        {
+            this.id = id;
+            this.device_id = device_id;
+            this.parameter = parameter;
+            this.min_value = min_value;
+            this.max_value = max_value;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {id}\nDevice ID: {device_id}\nParameter: {parameter}\nMin value: {min_value}\nMax value: {max_value}\n";
+        }
+
+    }
+}
